70649c3148fec6190e99b3f14d1ff392aeeb7e87
Mario Rovito <alessandro.r14@gmail.com>
2022-03-08 18:04:22 +0100

    add for 1-100

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ console.log('JS OK');
 
 // MILESTONE 3
 //Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+
+
+const square = document.querySelector('.squares')
+
+//creazione dei blocchi da 1  a 100
+
+for (let i = 1; i <= 100; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    div.append(i);
+    square.append(div);
+}
